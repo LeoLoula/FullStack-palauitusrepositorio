5f84c7f87a39277945bd6e362a7841e5e12f1783
Leo Loula <77982568+LeoLoula@users.noreply.github.com>
2022-08-09 15:52:59 +0300

    3.9-3.11

---
+++ Osa3/phonebook-front/src/components/persons.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import personService from '../services/persons'
+
+const Persons = ({ filteredPersons }) => {
+  return (
+    <div>
+      {filteredPersons.map((person) => (
+        <form
+          onSubmit={(e) => personService.deletePerson(person.id, person.name)}
+          key={person.id}
+        >
+          <p>
+            {person.name} {person.number}
+            <button type="submit">Delete</button>
+          </p>
+        </form>
+      ))}
+    </div>
+  )
+}
+
+export default Persons
